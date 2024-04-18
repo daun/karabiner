96b1d25de4b152998fc6976319befee4d38c2ffa
Philipp Daun <post@philippdaun.net>
2024-04-18 13:01:39 +0200

    Define Karabiner config

--- rules.ts
+++ rules.ts
@@ -283,7 +283,11 @@ fs.writeFileSync(
   JSON.stringify(
     {
       global: {
+        ask_for_confirmation_before_quitting: true,
+        check_for_updates_on_startup: true,
         show_in_menu_bar: false,
+        show_profile_name_in_menu_bar: false,
+        unsafe_ui: false
       },
       profiles: [
         {
