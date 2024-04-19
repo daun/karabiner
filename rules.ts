98dce713b525352566995aa0290fce46a507557e
Philipp Daun <post@philippdaun.net>
2024-04-19 11:28:38 +0200

    Clean up hyper key setup

--- rules.ts
+++ rules.ts
@@ -17,33 +17,10 @@ const rules: KarabinerRules[] = [
     manipulators: [
       {
         description: "Caps Lock -> Hyper Key",
-        from: {
-          key_code: "caps_lock",
-          modifiers: {
-            optional: ["any"],
-          },
-        },
-        to: [
-          {
-            set_variable: {
-              name: "hyper",
-              value: 1,
-            },
-          },
-        ],
-        to_after_key_up: [
-          {
-            set_variable: {
-              name: "hyper",
-              value: 0,
-            },
-          },
-        ],
-        to_if_alone: [
-          {
-            key_code: "escape",
-          },
-        ],
+        from: { key_code: "caps_lock", modifiers: { optional: ["any"] } },
+        to: [{ set_variable: { name: "hyper", value: 1 } }],
+        to_after_key_up: [{ set_variable: { name: "hyper", value: 0 } }],
+        to_if_alone: [{ key_code: "escape" }],
         type: "basic",
       },
     ],
